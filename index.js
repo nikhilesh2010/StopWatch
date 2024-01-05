f512cef5e9aa04da289f6f82d4ad37a89eb3fc1e
nikhilesh2010 <nikhilesh.cc@gmail.com>
2024-01-06 01:07:29 +0530

    Upload Files - First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+const stopwatch = () => {
+    seconds++;
+    if (seconds == 60)
+    {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60)
+        {
+            minutes = 0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+const watchStart = () => {
+    if (timer !== null)
+    {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+    
+}
+const watchStop = () => {
+    clearInterval(timer);
+    console.log(timer);
+}
+const watchReset = () => {
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00"
+}
