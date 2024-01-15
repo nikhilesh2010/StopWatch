7b473d46798918675e2b7d225b62af9532677f08
nikhilesh2010 <nikhilesh.cc@gmail.com>
2024-01-15 21:21:42 +0530

    Add Milliseconds

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [milliseconds, seconds, minutes, hours] = [0,0,0,0];
 let displayTime = document.getElementById("displayTime");
 let timer = null;
 
 const stopwatch = () => {
-    seconds++;
-    if (seconds == 60)
-    {
-        seconds = 0;
-        minutes++;
-        if (minutes == 60)
-        {
-            minutes = 0;
-            hours++;
+    milliseconds++;
+    if (milliseconds >= 100) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes >= 60) {
+                minutes = 0;
+                hours++;
+            }
         }
     }
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 10 ? "0" + milliseconds : milliseconds;
 
-    displayTime.innerHTML = h + ":" + m + ":" + s;
+    displayTime.innerHTML = h + ":" + m + ":" + s + ":" + ms;
 }
+
 const watchStart = () => {
     if (timer !== null)
     {
         clearInterval(timer);
     }
-    timer = setInterval(stopwatch,1000);
+    timer = setInterval(stopwatch,10);
     
 }
 const watchStop = () => {
@@ -34,6 +38,6 @@ const watchStop = () => {
 }
 const watchReset = () => {
     clearInterval(timer);
-    [seconds, minutes, hours] = [0,0,0];
-    displayTime.innerHTML = "00:00:00"
+    [milliseconds, seconds, minutes, hours] = [0,0,0,0];
+    displayTime.innerHTML = "00:00:00:00"
 }
